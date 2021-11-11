6fce93a6c46c222f1a7c58f0f222e263e672b3f4
Aron Eli baldvinsson <aronelibaldvinsson@arons-mbp.eduroam.net.au.dk>
2021-11-11 16:02:59 +0100

    added array something something

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -148,7 +148,7 @@ class CycleGANModel(BaseModel):
         fake_A = self.fake_A_pool.query(self.fake_A)
         self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, fake_A)
 
-    def tv_loss(img):
+    def tv_loss(img,placeholder):
         """
         Compute total variation loss.
         Inputs:
